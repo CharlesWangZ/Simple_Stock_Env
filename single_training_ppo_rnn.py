363987501caf9603f5d9172065e3489626e67ca5
CharlesWangZ <106153413+CharlesWangZ@users.noreply.github.com>
2024-06-18 00:10:59 +0100

    Update and rename experiment.py to single_training_ppo_rnn.py

---
+++ single_training_ppo_rnn.py
@@ -0,0 +1,420 @@
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+import numpy as np
+import optax
+from flax.linen.initializers import constant, orthogonal
+from typing import Sequence, NamedTuple, Any, Dict
+from flax.training.train_state import TrainState
+import distrax
+import gymnax
+import functools
+import matplotlib.pyplot as plt
+from flax import serialization
+import json
+import time
+
+# from single_stock_short import Stock_GBM
+from single_stock import Stock_GBM
+
+import wandb
+wandb.login()
+wandb.init(
+    project="single-learning-100-br",
+)
+
+network_size = 128
+hidden_size = 128
+
+class ScannedRNN(nn.Module):
+    @functools.partial(
+        nn.scan,
+        variable_broadcast="params",
+        in_axes=0,
+        out_axes=0,
+        split_rngs={"params": False},
+    )
+    @nn.compact
+    def __call__(self, carry, x):
+        """Applies the module."""
+        rnn_state = carry
+        ins, resets = x
+        features = carry[0].shape[-1]
+        rnn_state = jnp.where(
+            resets[:, np.newaxis],
+            self.initialize_carry(ins.shape[0], ins.shape[1]),
+            rnn_state,
+        )
+        new_rnn_state, y = nn.GRUCell(features)(rnn_state, ins)
+        return new_rnn_state, y
+    
+    @staticmethod
+    def initialize_carry(batch_size, hidden_size):
+        # Use a dummy key since the default state init fn is just zeros.
+        return nn.GRUCell(hidden_size, parent=None).initialize_carry(
+            jax.random.PRNGKey(0), (batch_size, hidden_size)
+        )
+    
+class ActorCriticRNN(nn.Module):
+    action_dim: Sequence[int]
+    config: Dict
+    @nn.compact
+    def __call__(self, hidden, x):
+        obs, dones = x
+        embedding = nn.Dense(
+            network_size, kernel_init=orthogonal(np.sqrt(2)), bias_init=constant(0.0)
+        )(obs)
+        embedding = nn.relu(embedding)
+
+        rnn_in = (embedding, dones)
+        hidden, embedding = ScannedRNN()(hidden, rnn_in)
+
+        actor_mean = nn.Dense(network_size, kernel_init=orthogonal(2), bias_init=constant(0.0))(
+            embedding
+        )
+        actor_mean = nn.relu(actor_mean)
+        actor_mean = nn.Dense(
+            self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0)
+        )(actor_mean)
+
+        pi = distrax.Categorical(logits=actor_mean)
+
+        critic = nn.Dense(network_size, kernel_init=orthogonal(2), bias_init=constant(0.0))(
+            embedding
+        )
+        critic = nn.relu(critic)
+        critic = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))(
+            critic
+        )
+
+        return hidden, pi, jnp.squeeze(critic, axis=-1)
+
+
+class Transition(NamedTuple):
+    done: jnp.ndarray
+    action: jnp.ndarray
+    value: jnp.ndarray
+    reward: jnp.ndarray
+    log_prob: jnp.ndarray
+    obs: jnp.ndarray
+    info: jnp.ndarray
+
+
+def make_train(config):
+    config["NUM_UPDATES"] = (
+        config["TOTAL_TIMESTEPS"] // config["NUM_STEPS"] // config["NUM_ENVS"]
+    )
+    print(config["NUM_UPDATES"])
+    config["MINIBATCH_SIZE"] = (
+        config["NUM_ENVS"] * config["NUM_STEPS"] // config["NUM_MINIBATCHES"]
+    )
+    env = Stock_GBM()
+    env_params = env.default_params
+
+    def linear_schedule(count):
+        frac = (
+            1.0
+            - (count // (config["NUM_MINIBATCHES"] * config["UPDATE_EPOCHS"]))
+            / config["NUM_UPDATES"]
+        )
+        return config["LR"] * frac
+
+    def train(rng):
+        # INIT NETWORK
+        network = ActorCriticRNN(env.action_space(env_params).n, config=config)
+        rng, _rng = jax.random.split(rng)
+        init_x = (
+            jnp.zeros(
+                (1, config["NUM_ENVS"], *env.observation_space(env_params).shape)
+            ),
+            jnp.zeros((1, config["NUM_ENVS"])),
+        )
+        init_hstate = ScannedRNN.initialize_carry(config["NUM_ENVS"], config["HIDDEN_SIZE"])
+        network_params = network.init(_rng, init_hstate, init_x)
+        if config["ANNEAL_LR"]:
+            tx = optax.chain(
+                optax.clip_by_global_norm(config["MAX_GRAD_NORM"]),
+                optax.adam(learning_rate=linear_schedule, eps=1e-5),
+            )
+        else:
+            tx = optax.chain(
+                optax.clip_by_global_norm(config["MAX_GRAD_NORM"]),
+                optax.adam(config["LR"], eps=1e-5),
+            )
+        train_state = TrainState.create(
+            apply_fn=network.apply,
+            params=network_params,
+            tx=tx,
+        )
+
+        # INIT ENV
+        rng, _rng = jax.random.split(rng)
+        reset_rng = jax.random.split(_rng, config["NUM_ENVS"])
+        obsv, env_state = jax.vmap(env.reset, in_axes=(0, None))(reset_rng, env_params)
+        init_hstate = ScannedRNN.initialize_carry(config["NUM_ENVS"], config["HIDDEN_SIZE"])
+
+        def _update_step(runner_state, unused):
+            # COLLECT TRAJECTORIES
+            def _env_step(runner_state, unused):
+                train_state, env_state, last_obs, last_done, hstate, rng = runner_state
+                rng, _rng = jax.random.split(rng)
+
+                # SELECT ACTION)
+                ac_in = (last_obs[np.newaxis, :], last_done[np.newaxis, :])
+                hstate, pi, value = network.apply(train_state.params, hstate, ac_in)
+                action = pi.sample(seed=_rng)
+                action = jnp.where(env_state.time_left <= 0, env_state.quant_remaining, action)
+                action = jnp.clip(action, 0, env_state.quant_remaining)
+                log_prob = pi.log_prob(action)
+                value, action, log_prob = (
+                    value.squeeze(0),
+                    action.squeeze(0),
+                    log_prob.squeeze(0),
+                )
+
+                # STEP ENV
+                rng, _rng = jax.random.split(rng)
+                rng_step = jax.random.split(_rng, config["NUM_ENVS"])
+                obsv, env_state, reward, done, info = jax.vmap(
+                    env.step, in_axes=(0, 0, 0, None)
+                )(rng_step, env_state, action, env_params)
+                transition = Transition(
+                    last_done, action, value, reward, log_prob, last_obs, info
+                )
+                runner_state = (train_state, env_state, obsv, done, hstate, rng)
+                return runner_state, transition
+
+            initial_hstate = runner_state[-2]
+            runner_state, traj_batch = jax.lax.scan(
+                _env_step, runner_state, None, config["NUM_STEPS"]
+            )
+
+            # CALCULATE ADVANTAGE
+            train_state, env_state, last_obs, last_done, hstate, rng = runner_state
+            ac_in = (last_obs[np.newaxis, :], last_done[np.newaxis, :])
+            _, _, last_val = network.apply(train_state.params, hstate, ac_in)
+            last_val = last_val.squeeze(0)
+            def _calculate_gae(traj_batch, last_val, last_done):
+                def _get_advantages(carry, transition):
+                    gae, next_value, next_done = carry
+                    done, value, reward = transition.done, transition.value, transition.reward 
+                    delta = reward + config["GAMMA"] * next_value * (1 - next_done) - value
+                    gae = delta + config["GAMMA"] * config["GAE_LAMBDA"] * (1 - next_done) * gae
+                    return (gae, value, done), gae
+                _, advantages = jax.lax.scan(_get_advantages, (jnp.zeros_like(last_val), last_val, last_done), traj_batch, reverse=True, unroll=16)
+                return advantages, advantages + traj_batch.value
+            advantages, targets = _calculate_gae(traj_batch, last_val, last_done)
+
+            # UPDATE NETWORK
+            def _update_epoch(update_state, unused):
+                def _update_minbatch(train_state, batch_info):
+                    init_hstate, traj_batch, advantages, targets = batch_info
+
+                    def _loss_fn(params, init_hstate, traj_batch, gae, targets):
+                        # RERUN NETWORK
+                        _, pi, value = network.apply(
+                            params, init_hstate[0], (traj_batch.obs, traj_batch.done)
+                        )
+                        log_prob = pi.log_prob(traj_batch.action)
+
+                        # CALCULATE VALUE LOSS
+                        value_pred_clipped = traj_batch.value + (
+                            value - traj_batch.value
+                        ).clip(-config["CLIP_EPS"], config["CLIP_EPS"])
+                        value_losses = jnp.square(value - targets)
+                        value_losses_clipped = jnp.square(value_pred_clipped - targets)
+                        value_loss = (
+                            0.5 * jnp.maximum(value_losses, value_losses_clipped).mean()
+                        )
+
+                        # CALCULATE ACTOR LOSS
+                        ratio = jnp.exp(log_prob - traj_batch.log_prob)
+                        gae = (gae - gae.mean()) / (gae.std() + 1e-8)
+                        loss_actor1 = ratio * gae
+                        loss_actor2 = (
+                            jnp.clip(
+                                ratio,
+                                1.0 - config["CLIP_EPS"],
+                                1.0 + config["CLIP_EPS"],
+                            )
+                            * gae
+                        )
+                        loss_actor = -jnp.minimum(loss_actor1, loss_actor2)
+                        loss_actor = loss_actor.mean()
+                        entropy = pi.entropy().mean()
+
+                        total_loss = (
+                            loss_actor
+                            + config["VF_COEF"] * value_loss
+                            - config["ENT_COEF"] * entropy
+                        )
+                        return total_loss, (value_loss, loss_actor, entropy)
+
+                    grad_fn = jax.value_and_grad(_loss_fn, has_aux=True)
+                    total_loss, grads = grad_fn(
+                        train_state.params, init_hstate, traj_batch, advantages, targets
+                    )
+                    train_state = train_state.apply_gradients(grads=grads)
+                    return train_state, total_loss
+
+                (
+                    train_state,
+                    init_hstate,
+                    traj_batch,
+                    advantages,
+                    targets,
+                    rng,
+                ) = update_state
+
+                rng, _rng = jax.random.split(rng)
+                permutation = jax.random.permutation(_rng, config["NUM_ENVS"])
+                batch = (init_hstate, traj_batch, advantages, targets)
+
+                shuffled_batch = jax.tree_util.tree_map(
+                    lambda x: jnp.take(x, permutation, axis=1), batch
+                )
+
+                minibatches = jax.tree_util.tree_map(
+                    lambda x: jnp.swapaxes(
+                        jnp.reshape(
+                            x,
+                            [x.shape[0], config["NUM_MINIBATCHES"], -1]
+                            + list(x.shape[2:]),
+                        ),
+                        1,
+                        0,
+                    ),
+                    shuffled_batch,
+                )
+
+                train_state, total_loss = jax.lax.scan(
+                    _update_minbatch, train_state, minibatches
+                )
+                update_state = (
+                    train_state,
+                    init_hstate,
+                    traj_batch,
+                    advantages,
+                    targets,
+                    rng,
+                )
+                return update_state, total_loss
+
+            init_hstate = initial_hstate[None, :]  # TBH
+            update_state = (
+                train_state,
+                init_hstate,
+                traj_batch,
+                advantages,
+                targets,
+                rng,
+            )
+            update_state, loss_info = jax.lax.scan(
+                _update_epoch, update_state, None, config["UPDATE_EPOCHS"]
+            )
+            train_state = update_state[0]
+            metric = (traj_batch.info,train_state.params)
+            rng = update_state[-1]
+
+            # Debugging mode
+            if config.get("DEBUG"):
+
+                def callback(metric):
+                    info, params = metric
+                    return_values = info["average_price"][info["done"]]
+                    timesteps = info["timestep"][info["done"]]
+                    action_at_execution = info["action"][info["done"]]
+                    for t in range(len(timesteps)):
+                        print(f"global step={timesteps[t]}, average price={return_values[t]}")
+                        wandb.log({"IS": return_values[t] - 100, "Optimal_IS": -5.95})
+                        # wandb.log({"Optimal_IS": -5.05})
+                        print(f"global step={timesteps[t]}, action_at_execution={action_at_execution[t]}")
+
+                    # timesteps = info["timestep"][info["done"]]
+                    # revenues = info["total_revenue"][info["done"]]
+                    # quant_executed = info["quant_executed"][info["done"]]
+                    # average_price = info["average_price"][info["done"]]
+                    # for t in range(len(timesteps)):  
+                    #     if wandbOn:
+                    #         wandb.log(
+                    #             {
+                    #                 "global_step": timesteps[t],
+                    #                 "episodic_revenue": revenues[t],
+                    #                 "quant_executed":quant_executed[t],
+                    #                 "average_price":average_price[t],
+                    #             }
+                    #         ) 
+                jax.debug.callback(callback, metric)
+
+            runner_state = (train_state, env_state, last_obs, last_done, hstate, rng)
+            return runner_state, traj_batch.info    
+                
+        rng, _rng = jax.random.split(rng)
+        runner_state = (
+            train_state,
+            env_state,
+            obsv,
+            jnp.zeros((config["NUM_ENVS"]), dtype=bool),
+            init_hstate,
+            _rng,
+        )
+        runner_state, metric = jax.lax.scan(
+            _update_step, runner_state, None, config["NUM_UPDATES"]
+        )
+        return {"runner_state": runner_state, "metrics": metric}
+
+    return train
+
+if __name__ == "__main__":
+    config = {
+        "LR": 1e-4,
+        "NUM_ENVS": 100,
+        "NUM_STEPS": 100,
+        "TOTAL_TIMESTEPS": 2e8,
+        "UPDATE_EPOCHS": 4,
+        "NUM_MINIBATCHES": 4,
+        "GAMMA": 1, 
+        "GAE_LAMBDA": 1,
+        "CLIP_EPS": 0.2,
+        "ENT_COEF": 0.05,
+        "VF_COEF": 0.5,
+        "MAX_GRAD_NORM": 0.5,
+        "ANNEAL_LR": True,
+        "DEBUG": True,
+        "HIDDEN_SIZE": hidden_size
+    }
+
+    rng = jax.random.PRNGKey(942)
+    start_time = time.time()
+    train_jit = jax.jit(make_train(config))
+    out = train_jit(rng)
+    end_time = time.time()
+    print(f"Execution time: {end_time - start_time} seconds")
+    info = out["metrics"]
+    # print(info)
+    # print(info["average_price"][info["done"]])
+    ave_price = info["average_price"][info["done"]]
+    plt.figure()  # Creates a new figure
+    plt.plot(ave_price)
+    plt.xlabel("Update Step")
+    plt.ylabel("average_price")
+    plt.show()
+    plt.savefig('average_price_traj.png')
+
+    print("DONE!!")
+    
+    train_state, env_state, last_obs, last_done, hstate, rng = out["runner_state"]
+
+    def save_params(params, file_path="params.pkl"):
+        with open(file_path, "wb") as f:
+            f.write(serialization.to_bytes(params))
+
+    save_params(train_state.params, "ppo_rnn_params.pkl")
+
+    for key, value in info.items():
+        info[key] = value.tolist()
+
+    with open('info.json', 'w') as json_file:
+        json.dump(info["action"], json_file)
+
+
